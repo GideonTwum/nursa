95246d49510f3b67d5f7b153028d52fdad5a9f8f
GideonTwum <twumgideonasre@gmail.com>
2026-01-27 22:48:09 +0000

    updated texts

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
                     <h1 className='text-white font-black text-5xl'>What is NUR<span className='text-yellow-500'>SA?</span></h1>
                 </div>
                 <div>
-                    <p className='text-white leading-loose'>Nursa stands for <span className='text-yellow-500'>Nursing and Midwifery students Association</span>. <br />
+                    <p className='text-[#ccc] leading-loose'>Nursa stands for <span className='text-yellow-500'>Nursing and Midwifery students Association</span>. <br />
                     Nursa is the official student body that represents all nursing and <br /> midwidery students at Valley View University. The association <br /> serves as a unifying platform that promotes academic excellence, <br /> professional growth, and student welfare within the School of Nursing <br /> and Midwifery. We serve as a bridge between students and faculty <br /> ensuring every voice is heard while promoting excellence, compassion, <br /> and integrity in healthcare training.
                     </p>
                 </div>
--- app/components/Contacts.jsx
+++ app/components/Contacts.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { MdEmail } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
+import { FaTiktok } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Contacts = () => {
   return (
@@ -11,7 +15,8 @@ const Contacts = () => {
          <p className='text-white text-sm'>We'd love to hear from you.</p>
         <div className='h-[4px] mt-4 bg-yellow-500 w-[5vw]'></div>
       </div>
-      <div className='mt-10 flex flex-col gap-4'>
+      <div className='mt-10 flex  gap-4'>
+        <div className='flex flex-col gap-4'>
             <div className='flex bg-green-500/30 p-5 rounded-lg w-[30vw] items-center gap-4'>
               <div className='p-2 bg-green-300/10'>
                   <MdEmail className='text-2xl text-yellow-500'/>
@@ -41,6 +46,22 @@ const Contacts = () => {
                 <p className='text-white text-sm'>Oyibi, Accra</p>
               </div>
             </div>
+        </div>
+
+        <div>
+            <div className='flex flex-col bg-green-500/30 p-5 rounded-lg w-[30vw] gap-4'>  
+              <div>
+                <p className='text-white text-xl'>Follow Us</p>
+              </div>
+              <div className='p-2  flex gap-4 w-[fit-content] rounded'>
+                  <FaTiktok className='text-2xl text-yellow-500 cursor-pointer'/>
+                  <FaInstagramSquare className='text-2xl text-yellow-500 cursor-pointer'/>
+                  <FaFacebook className='text-2xl text-yellow-500 cursor-pointer'/>
+                  <FaSquareXTwitter className='text-2xl text-yellow-500 cursor-pointer'/>
+              </div>
+            </div>          
+        </div>
+            
 
       </div>
     </div>
