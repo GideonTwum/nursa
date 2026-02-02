29e02d816c60cab7787e18c9f96816503e282631
GideonTwum <twumgideonasre@gmail.com>
2026-02-02 17:19:34 +0000

    responsiveness added

---
+++ app/shop/page.jsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react'
+import Nav from '../components/Nav'
+
+const page = () => {
+  return (
+    <div className='min-h-screen flex justify-center'>
+        <div className='flex flex-col items-center justify-center '>
+            <h1>Shop Page</h1>
+            <p className='text-green-700 font-bold'>There are no products available at the moment!</p>
+        </div>
+        
+    </div>
+  )
+}
+
+export default page
